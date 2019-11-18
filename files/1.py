eb30ca8f23130f50edfb161e409304a566e914d6
crazydiver <iven.gorby@gmail.com>
2019-11-18 17:13:09 +0300

    111

--- files/1.py
+++ files/1.py
@@ -1,6 +1,15 @@
 s = input().lower()
 ns = 0
-for i in s:
-    if i not in 'qwertyuiopasdfghjklzxcvbnm,.1234567890':
+i = 0
+while not s[i].isalpha():
+    s = s[i + 1:]
+    i += 1
+i = len(s) - 1
+while not s[i].isalpha():
+    s = s[: i]
+    i -= 1
+for i, j in enumerate(s):
+    if j.isalpha() and not s[i-1].isalpha():
         ns += 1
 print(ns + 1)
+# ..,/,./ecwvre ,./bweb ,/.
